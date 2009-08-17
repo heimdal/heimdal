13c3b9b1c6c3b3269f9bc42d1b7228c9f30bc89f
Love Hornquist Astrand <lha@h5l.org>
2009-08-17 10:15:31 +0200

    switch to use EVP interface instead of old MDX_ style interface

--- lib/gssapi/krb5/verify_mic.c
+++ lib/gssapi/krb5/verify_mic.c
@@ -46,7 +46,7 @@ verify_mic_des
 	    )
 {
   u_char *p;
-  MD5_CTX md5;
+  EVP_MD_CTX md5;
   u_char hash[16], *seq;
   DES_key_schedule schedule;
   DES_cblock zero;
@@ -72,11 +72,12 @@ verify_mic_des
   p += 16;
 
   /* verify checksum */
-  MD5_Init (&md5);
-  MD5_Update (&md5, p - 24, 8);
-  MD5_Update (&md5, message_buffer->value,
-	     message_buffer->length);
-  MD5_Final (hash, &md5);
+  EVP_MD_CTX_init(&md5);
+  EVP_DigestInit_ex(&md5, EVP_md5(), NULL);
+  EVP_DigestUpdate(&md5, p - 24, 8);
+  EVP_DigestUpdate(&md5, message_buffer->value, message_buffer->length);
+  EVP_DigestFinal_ex(&md5, hash, NULL);
+  EVP_MD_CTX_cleanup(&md5);
 
   memset (&zero, 0, sizeof(zero));
   memcpy (&deskey, key->keyvalue.data, sizeof(deskey));

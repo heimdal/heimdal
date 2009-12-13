356a2048a579c9bf3b3b924b643210e806fd3377
Love Hornquist Astrand <lha@h5l.org>
2009-12-13 12:24:47 -0800

    less dup rk_

--- lib/krb5/auth_context.c
+++ lib/krb5/auth_context.c
@@ -173,7 +173,7 @@ krb5_auth_con_genaddrs(krb5_context context,
 	    if(getsockname(fd, local, &len) < 0) {
 		char buf[128];
 		ret = errno;
-		rk_rk_strerror_r(ret, buf, sizeof(buf));
+		rk_strerror_r(ret, buf, sizeof(buf));
 		krb5_set_error_message(context, ret, "getsockname: %s", buf);
 		goto out;
 	    }
@@ -191,11 +191,7 @@ krb5_auth_con_genaddrs(krb5_context context,
 	if(getpeername(fd, remote, &len) < 0) {
 	    char buf[128];
 	    ret = errno;
-<<<<<<< HEAD:lib/krb5/auth_context.c
 	    rk_strerror_r(ret, buf, sizeof(buf));
-=======
-	    rk_rk_strerror_r(ret, buf, sizeof(buf));
->>>>>>> 75aa4b4... switch to rk_strerror_r:lib/krb5/auth_context.c
 	    krb5_set_error_message(context, ret, "getpeername: %s", buf);
 	    goto out;
 	}

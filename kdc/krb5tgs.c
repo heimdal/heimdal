24e2001f512c73dcffad1cb09e9b12493972faec
Love Hornquist Astrand <lha@h5l.org>
2010-03-19 13:56:20 -0700

    support old SIGNTICKET too

--- kdc/krb5tgs.c
+++ kdc/krb5tgs.c
@@ -88,7 +88,8 @@ find_KRB5SignedPath(krb5_context context,
 	return KRB5KDC_ERR_PADATA_TYPE_NOSUPP;
     }
 
-    if (child.val[0].ad_type != KRB5_AUTHDATA_SIGNTICKET) {
+    if (child.val[0].ad_type != KRB5_AUTHDATA_SIGNTICKET && 
+	child.val[0].ad_type != KRB5_AUTHDATA_SIGNTICKET_OLD) {
 	free_AuthorizationData(&child);
 	return KRB5KDC_ERR_PADATA_TYPE_NOSUPP;
     }

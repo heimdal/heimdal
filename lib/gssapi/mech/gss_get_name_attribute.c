95e08279269e9fedb0321493dd3cdc30f5c02db1
Love Hornquist Astrand <lha@h5l.org>
2010-08-02 21:15:56 -0700

    add gss_get_name_attribute

---
+++ lib/gssapi/mech/gss_get_name_attribute.c
@@ -0,0 +1,17 @@
+
+/**
+ *
+ */
+
+OM_uint32
+gss_get_name_attribute(OM_uint32 *minor_status,
+		       gss_name_t name,
+		       gss_buffer_t attr,
+		       int *authenticated,
+		       int  *complete,
+		       gss_buffer_t value,
+		       gss_buffer_t display_value,
+		       int *more)
+{
+    return GSS_S_FAILURE;
+}

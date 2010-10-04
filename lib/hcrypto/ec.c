34e5278ae426880e2d9ed1494338fdb5d2e8e707
Love Hornquist Astrand <lha@h5l.org>
2010-10-04 00:03:12 -0700

    random bits

--- lib/hcrypto/ec.c
+++ lib/hcrypto/ec.c
@@ -33,12 +33,26 @@
 
 #include "ec.h"
 
-struct EC_KEY {
-    int foo;
+struct EC_POINT {
+    int inf;
+    mp_int x;
+    mp_int y;
+    mp_int z;
 };
 
 struct EC_GROUP {
-    int foo;
+    size_t size;
+    mp_int prime;
+    mp_int order;
+    mp_int Gx;
+    mp_int Gy;
+};
+
+struct EC_KEY {
+    int type;
+    EC_GROUP *group;
+    EC_POINT *pubkey;
+    mp_int privkey;
 };
 
 
@@ -73,6 +87,67 @@ EC_GROUP_new_by_curve_name(int nid)
 {
 }
 
+EC_KEY *
+EC_KEY_new_by_curve_name(EC_GROUP_ID nid)
+{
+    EC_KEY *key;
+    
+    key = calloc(1, sizeof(*key));
+    return key;
+}
+
+void
+EC_POINT_free(EC_POINT *p)
+{
+    mp_clear_multi(&p->x, p->y, p->z, NULL);
+    free(p);
+}
+
+static int
+ec_point_mul(EC_POINT *res, const EC_GROUP *group, const mp_int *point)
+{
+}
+
+EC_POINT *
+EC_POINT_new(void)
+{
+    EC_POINT *p;
+
+    p = calloc(1, sizeof(*p));
+
+    if (mp_init_multi(&p->x, &p->y, &p->z, NULL) != 0) {
+	EC_POINT_free(p);
+	return NULL;
+    }
+
+    return p;
+}
+
+int
+EC_KEY_generate_key(EC_KEY *key)
+{
+    int ret = 0;
+
+    if (key->group == NULL)
+	return 0;
+
+    do {
+	random(key->privkey, key->group->size);
+    } while(mp_cmp(key->privkey, key->group->order) >= 0);
+
+    if (key->pubkey == NULL)
+	key->pubkey = EC_POINT_new();
+
+    if (ec_point_mul(&key->pubkey, key->group, key->privkey) != 1)
+	goto error;
+
+    ret = 1;
+ error:
+    ECPOINT_free(&base);
+
+    return ret;
+}
+
 void
 EC_KEY_set_group(EC_KEY *, EC_GROUP *)
 {
@@ -89,11 +164,12 @@ EC_KEY_check_key(const EC_KEY *)
 {
 }
 
-const BIGNUM 
-EC_KEY_get0_private_key(const EC_KEY *)
+const BIGNUM *
+EC_KEY_get0_private_key(const EC_KEY *key)
 {
 }
 
-int EC_KEY_set_private_key(EC_KEY *, const BIGNUM *)
+int
+EC_KEY_set_private_key(EC_KEY *key, const BIGNUM *bn)
 {
 }

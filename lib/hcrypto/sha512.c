8e7cf164918daf1264c3660522259e66182ee463
Love Hornquist Astrand <lha@h5l.org>
2010-10-11 20:15:37 -0700

    Switch to ULL

--- lib/hcrypto/sha512.c
+++ lib/hcrypto/sha512.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006 Kungliga Tekniska Högskolan
+ * Copyright (c) 2006, 2010 Kungliga Tekniska Högskolan
  * (Royal Institute of Technology, Stockholm, Sweden).
  * All rights reserved.
  *
@@ -56,46 +56,46 @@
 #define H m->counter[7]
 
 static const uint64_t constant_512[80] = {
-    0x428a2f98d728ae22LL, 0x7137449123ef65cdLL,
-    0xb5c0fbcfec4d3b2fLL, 0xe9b5dba58189dbbcLL,
-    0x3956c25bf348b538LL, 0x59f111f1b605d019LL,
-    0x923f82a4af194f9bLL, 0xab1c5ed5da6d8118LL,
-    0xd807aa98a3030242LL, 0x12835b0145706fbeLL,
-    0x243185be4ee4b28cLL, 0x550c7dc3d5ffb4e2LL,
-    0x72be5d74f27b896fLL, 0x80deb1fe3b1696b1LL,
-    0x9bdc06a725c71235LL, 0xc19bf174cf692694LL,
-    0xe49b69c19ef14ad2LL, 0xefbe4786384f25e3LL,
-    0x0fc19dc68b8cd5b5LL, 0x240ca1cc77ac9c65LL,
-    0x2de92c6f592b0275LL, 0x4a7484aa6ea6e483LL,
-    0x5cb0a9dcbd41fbd4LL, 0x76f988da831153b5LL,
-    0x983e5152ee66dfabLL, 0xa831c66d2db43210LL,
-    0xb00327c898fb213fLL, 0xbf597fc7beef0ee4LL,
-    0xc6e00bf33da88fc2LL, 0xd5a79147930aa725LL,
-    0x06ca6351e003826fLL, 0x142929670a0e6e70LL,
-    0x27b70a8546d22ffcLL, 0x2e1b21385c26c926LL,
-    0x4d2c6dfc5ac42aedLL, 0x53380d139d95b3dfLL,
-    0x650a73548baf63deLL, 0x766a0abb3c77b2a8LL,
-    0x81c2c92e47edaee6LL, 0x92722c851482353bLL,
-    0xa2bfe8a14cf10364LL, 0xa81a664bbc423001LL,
-    0xc24b8b70d0f89791LL, 0xc76c51a30654be30LL,
-    0xd192e819d6ef5218LL, 0xd69906245565a910LL,
-    0xf40e35855771202aLL, 0x106aa07032bbd1b8LL,
-    0x19a4c116b8d2d0c8LL, 0x1e376c085141ab53LL,
-    0x2748774cdf8eeb99LL, 0x34b0bcb5e19b48a8LL,
-    0x391c0cb3c5c95a63LL, 0x4ed8aa4ae3418acbLL,
-    0x5b9cca4f7763e373LL, 0x682e6ff3d6b2b8a3LL,
-    0x748f82ee5defb2fcLL, 0x78a5636f43172f60LL,
-    0x84c87814a1f0ab72LL, 0x8cc702081a6439ecLL,
-    0x90befffa23631e28LL, 0xa4506cebde82bde9LL,
-    0xbef9a3f7b2c67915LL, 0xc67178f2e372532bLL,
-    0xca273eceea26619cLL, 0xd186b8c721c0c207LL,
-    0xeada7dd6cde0eb1eLL, 0xf57d4f7fee6ed178LL,
-    0x06f067aa72176fbaLL, 0x0a637dc5a2c898a6LL,
-    0x113f9804bef90daeLL, 0x1b710b35131c471bLL,
-    0x28db77f523047d84LL, 0x32caab7b40c72493LL,
-    0x3c9ebe0a15c9bebcLL, 0x431d67c49c100d4cLL,
-    0x4cc5d4becb3e42b6LL, 0x597f299cfc657e2aLL,
-    0x5fcb6fab3ad6faecLL, 0x6c44198c4a475817LL
+    0x428a2f98d728ae22ULL, 0x7137449123ef65cdULL,
+    0xb5c0fbcfec4d3b2fULL, 0xe9b5dba58189dbbcULL,
+    0x3956c25bf348b538ULL, 0x59f111f1b605d019ULL,
+    0x923f82a4af194f9bULL, 0xab1c5ed5da6d8118ULL,
+    0xd807aa98a3030242ULL, 0x12835b0145706fbeULL,
+    0x243185be4ee4b28cULL, 0x550c7dc3d5ffb4e2ULL,
+    0x72be5d74f27b896fULL, 0x80deb1fe3b1696b1ULL,
+    0x9bdc06a725c71235ULL, 0xc19bf174cf692694ULL,
+    0xe49b69c19ef14ad2ULL, 0xefbe4786384f25e3ULL,
+    0x0fc19dc68b8cd5b5ULL, 0x240ca1cc77ac9c65ULL,
+    0x2de92c6f592b0275ULL, 0x4a7484aa6ea6e483ULL,
+    0x5cb0a9dcbd41fbd4ULL, 0x76f988da831153b5ULL,
+    0x983e5152ee66dfabULL, 0xa831c66d2db43210ULL,
+    0xb00327c898fb213fULL, 0xbf597fc7beef0ee4ULL,
+    0xc6e00bf33da88fc2ULL, 0xd5a79147930aa725ULL,
+    0x06ca6351e003826fULL, 0x142929670a0e6e70ULL,
+    0x27b70a8546d22ffcULL, 0x2e1b21385c26c926ULL,
+    0x4d2c6dfc5ac42aedULL, 0x53380d139d95b3dfULL,
+    0x650a73548baf63deULL, 0x766a0abb3c77b2a8ULL,
+    0x81c2c92e47edaee6ULL, 0x92722c851482353bULL,
+    0xa2bfe8a14cf10364ULL, 0xa81a664bbc423001ULL,
+    0xc24b8b70d0f89791ULL, 0xc76c51a30654be30ULL,
+    0xd192e819d6ef5218ULL, 0xd69906245565a910ULL,
+    0xf40e35855771202aULL, 0x106aa07032bbd1b8ULL,
+    0x19a4c116b8d2d0c8ULL, 0x1e376c085141ab53ULL,
+    0x2748774cdf8eeb99ULL, 0x34b0bcb5e19b48a8ULL,
+    0x391c0cb3c5c95a63ULL, 0x4ed8aa4ae3418acbULL,
+    0x5b9cca4f7763e373ULL, 0x682e6ff3d6b2b8a3ULL,
+    0x748f82ee5defb2fcULL, 0x78a5636f43172f60ULL,
+    0x84c87814a1f0ab72ULL, 0x8cc702081a6439ecULL,
+    0x90befffa23631e28ULL, 0xa4506cebde82bde9ULL,
+    0xbef9a3f7b2c67915ULL, 0xc67178f2e372532bULL,
+    0xca273eceea26619cULL, 0xd186b8c721c0c207ULL,
+    0xeada7dd6cde0eb1eULL, 0xf57d4f7fee6ed178ULL,
+    0x06f067aa72176fbaULL, 0x0a637dc5a2c898a6ULL,
+    0x113f9804bef90daeULL, 0x1b710b35131c471bULL,
+    0x28db77f523047d84ULL, 0x32caab7b40c72493ULL,
+    0x3c9ebe0a15c9bebcULL, 0x431d67c49c100d4cULL,
+    0x4cc5d4becb3e42b6ULL, 0x597f299cfc657e2aULL,
+    0x5fcb6fab3ad6faecULL, 0x6c44198c4a475817ULL
 };
 
 void
@@ -103,14 +103,14 @@ SHA512_Init (SHA512_CTX *m)
 {
     m->sz[0] = 0;
     m->sz[1] = 0;
-    A = 0x6a09e667f3bcc908LL;
-    B = 0xbb67ae8584caa73bLL;
-    C = 0x3c6ef372fe94f82bLL;
-    D = 0xa54ff53a5f1d36f1LL;
-    E = 0x510e527fade682d1LL;
-    F = 0x9b05688c2b3e6c1fLL;
-    G = 0x1f83d9abfb41bd6bLL;
-    H = 0x5be0cd19137e2179LL;
+    A = 0x6a09e667f3bcc908ULL;
+    B = 0xbb67ae8584caa73bULL;
+    C = 0x3c6ef372fe94f82bULL;
+    D = 0xa54ff53a5f1d36f1ULL;
+    E = 0x510e527fade682d1ULL;
+    F = 0x9b05688c2b3e6c1fULL;
+    G = 0x1f83d9abfb41bd6bULL;
+    H = 0x5be0cd19137e2179ULL;
 }
 
 static void
@@ -172,10 +172,10 @@ swap_uint64_t (uint64_t t)
     uint64_t temp;
 
     temp   = cshift64(t, 32);
-    temp = ((temp & 0xff00ff00ff00ff00LL) >> 8) |
-           ((temp & 0x00ff00ff00ff00ffLL) << 8);
-    return ((temp & 0xffff0000ffff0000LL) >> 16) |
-           ((temp & 0x0000ffff0000ffffLL) << 16);
+    temp = ((temp & 0xff00ff00ff00ff00ULL) >> 8) |
+           ((temp & 0x00ff00ff00ff00ffULL) << 8);
+    return ((temp & 0xffff0000ffff0000ULL) >> 16) |
+           ((temp & 0x0000ffff0000ffffULL) << 16);
 }
 #endif
 
@@ -268,14 +268,14 @@ SHA384_Init(SHA384_CTX *m)
 {
     m->sz[0] = 0;
     m->sz[1] = 0;
-    A = 0xcbbb9d5dc1059ed8LL;
-    B = 0x629a292a367cd507LL;
-    C = 0x9159015a3070dd17LL;
-    D = 0x152fecd8f70e5939LL;
-    E = 0x67332667ffc00b31LL;
-    F = 0x8eb44a8768581511LL;
-    G = 0xdb0c2e0d64f98fa7LL;
-    H = 0x47b5481dbefa4fa4LL;
+    A = 0xcbbb9d5dc1059ed8ULL;
+    B = 0x629a292a367cd507ULL;
+    C = 0x9159015a3070dd17ULL;
+    D = 0x152fecd8f70e5939ULL;
+    E = 0x67332667ffc00b31ULL;
+    F = 0x8eb44a8768581511ULL;
+    G = 0xdb0c2e0d64f98fa7ULL;
+    H = 0x47b5481dbefa4fa4ULL;
 }
 
 void

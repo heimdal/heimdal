1c78d993232b78ec9439b301c8e6bd9e59a0a451
Joerg Pulz <Joerg.Pulz@frm2.tum.de>
2010-10-18 08:28:42 -0700

    Make build w/o PKINIT
    Signed-off-by: Love Hornquist Astrand <lha@h5l.org>

--- kdc/config.c
+++ kdc/config.c
@@ -333,6 +333,7 @@ configure(krb5_context context, int argc, char **argv)
 
     krb5_kdc_windc_init(context);
 
+#ifdef PKINIT
 #ifdef __APPLE__
     config->enable_pkinit = 1;
 
@@ -345,7 +346,7 @@ configure(krb5_context context, int argc, char **argv)
     if (config->pkinit_kdc_anchors == NULL)
 	config->pkinit_kdc_anchors = strdup("KEYCHAIN:");
 
-#endif
+#endif /* __APPLE__ */
 
     if (config->enable_pkinit) {
 	if (config->pkinit_kdc_identity == NULL)
@@ -361,6 +362,8 @@ configure(krb5_context context, int argc, char **argv)
 			       config->pkinit_kdc_revoke);
 
     }
+
+#endif /* PKINIT */
     
     return config;
 }
--- kdc/kdc-replay.c
+++ kdc/kdc-replay.c
@@ -86,6 +86,7 @@ main(int argc, char **argv)
     if (ret)
 	krb5_err(context, 1, ret, "krb5_kdc_set_dbinfo");
 
+#ifdef PKINIT
     if (config->enable_pkinit) {
 	if (config->pkinit_kdc_identity == NULL)
 	    krb5_errx(context, 1, "pkinit enabled but no identity");
@@ -100,6 +101,7 @@ main(int argc, char **argv)
 			       config->pkinit_kdc_revoke);
 
     }
+#endif /* PKINIT */
 
     if (argc != 2)
 	errx(1, "argc != 2");

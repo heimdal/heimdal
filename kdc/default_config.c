f93a56f931e91c7b8eeec83b94015604bbc898f1
Nicolas Williams <nico103@gmail.com>
2011-05-30 22:32:16 -0500

    Set improved enctypes parameter defaults to better match the RFC.
    Signed-off-by: Love Hörnquist Åstrand <lha@h5l.org>

--- kdc/default_config.c
+++ kdc/default_config.c
@@ -51,10 +51,10 @@ krb5_kdc_get_config(krb5_context context, krb5_kdc_configuration **config)
     c->require_preauth = TRUE;
     c->kdc_warn_pwexpire = 0;
     c->encode_as_rep_as_tgs_rep = FALSE;
-    c->as_use_strongest_session_key = TRUE;
-    c->preauth_use_strongest_session_key = TRUE;
-    c->tgs_use_strongest_session_key = TRUE;
-    c->use_strongest_server_key = TRUE;
+    c->as_use_strongest_session_key = FALSE;
+    c->preauth_use_strongest_session_key = FALSE;
+    c->tgs_use_strongest_session_key = FALSE;
+    c->use_strongest_server_key = FALSE;
     c->check_ticket_addresses = TRUE;
     c->allow_null_ticket_addresses = TRUE;
     c->allow_anonymous = FALSE;

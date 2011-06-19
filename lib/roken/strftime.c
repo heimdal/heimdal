4337582a644d31b1e230b8b0c801f7f10c950c8f
Love Hörnquist Åstrand <lha@h5l.org>
2011-06-19 10:28:51 -0700

    add missing break, quiet clang analyzer

--- lib/roken/strftime.c
+++ lib/roken/strftime.c
@@ -286,7 +286,7 @@ strftime (char *buf, size_t maxsize, const char *format,
 				"%02d:%02d",
 				tm->tm_hour,
 				tm->tm_min);
-
+		break;
 	    case 's' :
 		ret = snprintf (buf, maxsize - n,
 				"%d", (int)mktime(rk_UNCONST(tm)));
@@ -392,6 +392,6 @@ strftime (char *buf, size_t maxsize, const char *format,
 	    ++n;
 	}
     }
-    *buf++ = '\0';
+    *buf = '\0';
     return n;
 }

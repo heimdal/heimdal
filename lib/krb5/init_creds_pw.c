b6fc70019ebfe7bd48073bf2405c8a2178b3a627
Love Hörnquist Åstrand <lha@h5l.org>
2011-07-24 22:33:39 -0700

    better error message

--- lib/krb5/init_creds_pw.c
+++ lib/krb5/init_creds_pw.c
@@ -1662,7 +1662,8 @@ check_fast(krb5_context context, struct fast_state *state)
 {
     if (state->flags & KRB5_FAST_EXPECTED) {
 	krb5_set_error_message(context, KRB5KRB_AP_ERR_MODIFIED,
-			       "Expected FAST not not found");
+			       "Expected FAST, but no FAST "
+			       "was in the response from the KDC");
 	return KRB5KRB_AP_ERR_MODIFIED;
     }
     return 0;

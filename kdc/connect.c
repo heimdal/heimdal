0055626fef8f950493b2cb49765ff8ba215d4079
Dana Koch <dsk@google.com>
2012-11-22 11:20:48 +0800

    Fix typo with return values in realloc_descrs.
    Signed-off-by: Love Hörnquist Åstrand <lha@h5l.org>

--- kdc/connect.c
+++ kdc/connect.c
@@ -846,7 +846,7 @@ realloc_descrs(struct descr **d, unsigned int *ndescr)
 
     tmp = realloc(*d, (*ndescr + 4) * sizeof(**d));
     if(tmp == NULL)
-        return TRUE;
+        return FALSE;
 
     *d = tmp;
     reinit_descrs (*d, *ndescr);
@@ -856,7 +856,7 @@ realloc_descrs(struct descr **d, unsigned int *ndescr)
 
     *ndescr += 4;
 
-    return FALSE;
+    return TRUE;
 }
 
 int

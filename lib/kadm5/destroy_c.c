6c4423cd46ae465393db97defcfabe187311b155
Jeffrey Altman <jaltman@secure-endpoints.com>
2014-02-03 15:40:32 -0500

    kadm5: use rk_closesocket to close sockets
    kadm5_c_destroy did not use rk_closesocket when cleaning up
    the context.  This results in an exception on Windows since a
    socket is not a file descriptor.
    
    Change-Id: I9ebddad61f0199acb495a0773925df4f41e4fef2

--- lib/kadm5/destroy_c.c
+++ lib/kadm5/destroy_c.c
@@ -42,7 +42,7 @@ kadm5_c_destroy(void *server_handle)
 
     free(context->realm);
     free(context->admin_server);
-    close(context->sock);
+    rk_closesocket(context->sock);
     if (context->client_name)
 	free(context->client_name);
     if (context->service_name)

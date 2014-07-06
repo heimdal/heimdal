a2ccf8b32e40e2c81691d0f9b68601bf057bf6aa
Chaskiel Grundman <cg2v@andrew.cmu.edu>
2014-07-06 14:37:49 -0400

    Document logic in _krb5_is_anon_request
    describe why we look at the different bits and fields

--- kdc/kerberos5.c
+++ kdc/kerberos5.c
@@ -2329,6 +2329,10 @@ _kdc_tkt_add_if_relevant_ad(krb5_context context,
 krb5_boolean
 _kdc_is_anon_request(const KDC_REQ_BODY *b)
 {
+	/* some versions of heimdal use bit 14 instead of 16 for
+	   request_anonymous, as indicated in the anonymous draft prior to
+	   version 11. Bit 14 is assigned to S4U2Proxy, but all S4U2Proxy
+	   requests will have a second ticket; don't consider those anonymous */
 	return (b->kdc_options.request_anonymous ||
 		(b->kdc_options.constrained_delegation && !b->additional_tickets));
 }

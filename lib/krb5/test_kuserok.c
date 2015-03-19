86017e8798e81b3be95884cf6171acf37a545972
Nicolas Williams <nico@cryptonector.com>
2015-03-19 09:29:55 -0500

    Fix leaks in test_kuserok.c

--- lib/krb5/test_kuserok.c
+++ lib/krb5/test_kuserok.c
@@ -96,6 +96,8 @@ main(int argc, char **argv)
 
     ret = krb5_kuserok(context, principal, argv[1]);
 
+    free(p);
+    krb5_free_principal(context, principal);
     krb5_free_context(context);
 
     printf("%s is %sallowed to login as %s\n", p, ret ? "" : "NOT ", argv[1]);

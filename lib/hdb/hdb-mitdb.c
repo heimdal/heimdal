f34394fe6974bab0a62a521efb5e64890753a440
Daria Phoebe Brashear <dariaphoebe@auristor.com>
2017-10-19 14:06:12 -0500

    hdb-mitdb: correct missing equals in initialization
    (cherry picked from commit 3bcc031939d4f8cf600839d1e92b252c250d4d79)
    
    Change-Id: I451c33b4caa58f06da3ed6a02a3f717323fc679e

--- lib/hdb/hdb-mitdb.c
+++ lib/hdb/hdb-mitdb.c
@@ -1116,7 +1116,7 @@ krb5_error_code
 hdb_mitdb_create(krb5_context context, HDB **db,
 		 const char *filename)
 {
-    MITDB **mdb (MITDB **)db;
+    MITDB **mdb = (MITDB **)db;
     *mdb = calloc(1, sizeof(**mdb));
     if (*mdb == NULL) {
 	krb5_set_error_message(context, ENOMEM, "malloc: out of memory");

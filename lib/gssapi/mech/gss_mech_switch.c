heimdal/heimdal

commit 4a93c4774a597f58546e631a3699b8ad0775dce5

gss: intern OIDs (#447)
Intern OIDs so that gss_release_oid() can be a NOOP.

lib/gssapi/mech/gss_mech_switch.c
@@ -43,15 +43,15 @@ static HEIMDAL_MUTEX _gss_mech_mutex = HEIMDAL_MUTEX_INITIALIZER;
  * (e.g. 1.2.840.113554.1.2.2) to a gss_OID.
  */
 static int
-_gss_string_to_oid(const char* s, gss_OID oid)
+_gss_string_to_oid(const char* s, gss_OID *oidp)
 {
 	int			number_count, i, j;
 	size_t			byte_count;
 	const char		*p, *q;
 	char			*res;
+	gss_OID_desc		oid;
 
-	oid->length = 0;
-	oid->elements = NULL;
+	*oidp = GSS_C_NO_OID;
 
 	/*
 	 * First figure out how many numbers in the oid, then
@@ -142,9 +142,20 @@ _gss_string_to_oid(const char* s, gss_OID oid)
 			res = malloc(byte_count);
 			if (!res)
 				return (ENOMEM);
-			oid->length = byte_count;
-			oid->elements = res;
+			oid.length = byte_count;
+			oid.elements = res;
 		}
+	}
+
+	{
+		OM_uint32 minor_status, tmp;
+
+		if (GSS_ERROR(_gss_intern_oid(&minor_status, &oid, oidp))) {
+			_gss_free_oid(&tmp, &oid);
+			return (minor_status);
+		}
+
+		_gss_free_oid(&tmp, &oid);
 	}
 
 	return (0);
@@ -205,7 +216,9 @@ add_builtin(gssapi_mech_interface mech)
 	return ENOMEM;
     m->gm_so = NULL;
     m->gm_mech = *mech;
-    m->gm_mech_oid = mech->gm_mech_oid; /* XXX */
+    _gss_intern_oid(&minor_status, &mech->gm_mech_oid, &m->gm_mech_oid);
+    if (minor_status)
+	return minor_status;
     gss_add_oid_set_member(&minor_status,
 			   &m->gm_mech.gm_mech_oid, &_gss_mech_oids);
 
@@ -236,7 +249,7 @@ _gss_load_mech(void)
 	char		*name, *oid, *lib, *kobj;
 	struct _gss_mech_switch *m;
 	void		*so;
-	gss_OID_desc	mech_oid;
+	gss_OID 	mech_oid;
 	int		found;
 #endif
 
@@ -291,9 +304,8 @@ _gss_load_mech(void)
 		 */
 		found = 0;
 		HEIM_SLIST_FOREACH(m, &_gss_mechs, gm_link) {
-			if (gss_oid_equal(&m->gm_mech.gm_mech_oid, &mech_oid)) {
+			if (gss_oid_equal(&m->gm_mech.gm_mech_oid, mech_oid)) {
 				found = 1;
-				free(mech_oid.elements);
 				break;
 			}
 		}
@@ -321,7 +333,7 @@ _gss_load_mech(void)
 		m->gm_so = so;
 		m->gm_mech_oid = mech_oid;
 		m->gm_mech.gm_name = strdup(name);
-		m->gm_mech.gm_mech_oid = mech_oid;
+		m->gm_mech.gm_mech_oid = *mech_oid;
 		m->gm_mech.gm_flags = 0;
 		m->gm_mech.gm_compat = calloc(1, sizeof(struct gss_mech_compat_desc_struct));
 		if (m->gm_mech.gm_compat == NULL)
@@ -392,7 +404,7 @@ _gss_load_mech(void)
 
 		mi = dlsym(so, "gss_mo_init");
 		if (mi != NULL) {
-			major_status = mi(&minor_status, &mech_oid,
+			major_status = mi(&minor_status, mech_oid,
 					  &m->gm_mech.gm_mo, &m->gm_mech.gm_mo_num);
 			if (GSS_ERROR(major_status))
 				goto bad;
@@ -419,7 +431,7 @@ _gss_load_mech(void)
 	bad:
 		if (m != NULL) {
 			free(m->gm_mech.gm_compat);
-			free(m->gm_mech.gm_mech_oid.elements);
+			/* do not free OID, it has been interned */
 			free((char *)m->gm_mech.gm_name);
 			free(m);
 		}

lib/gssapi/gen-oid.pl
@@ -112,6 +112,7 @@ while(<>) {
 	    printf "/* $name - $oid */\n";
 	    printf "gss_OID_desc GSSAPI_LIB_VARIABLE $store = { $length, rk_UNCONST(\"$data\") };\n\n";
 	}
+	push(@oidstorage, $store);
     } elsif (/^desc\s+([\w]+)\s+(\w+)\s+(\"[^\"]*\")\s+(\"[^\"]*\")/) {
         my ($type, $oid, $short, $long) = ($1, $2, $3, $4);
 	my $object = { type=> $type, oid => $oid, short => $short, long => $long };
@@ -138,4 +139,11 @@ foreach my $k (sort keys %types) {
 
 if ($header) {
     printf "#endif /* GSSAPI_GSSAPI_OID */\n";
+} else {
+    printf "gss_OID _gss_ot_internal[] = {\n";
+    foreach my $k (@oidstorage) {
+	print "  &$k,\n";
+    }
+    printf "};\n\n";
+    printf "size_t _gss_ot_internal_count = sizeof(_gss_ot_internal) / sizeof(_gss_ot_internal[0]);\n";
 }

lib/gssapi/mech/gss_authorize_localname.c
@@ -56,7 +56,7 @@ mech_authorize_localname(OM_uint32 *minor_status,
         major_status = m->gm_authorize_localname(minor_status,
                                                  mn->gmn_name,
                                                  &user->gn_value,
-                                                 &user->gn_type);
+                                                 user->gn_type);
         if (major_status != GSS_S_UNAUTHORIZED)
             break;
     }
@@ -75,7 +75,7 @@ attr_authorize_localname(OM_uint32 *minor_status,
     OM_uint32 major_status = GSS_S_UNAVAILABLE;
     int more = -1;
 
-    if (!gss_oid_equal(&user->gn_type, GSS_C_NT_USER_NAME))
+    if (!gss_oid_equal(user->gn_type, GSS_C_NT_USER_NAME))
         return GSS_S_BAD_NAMETYPE;
 
     while (more != 0 && major_status != GSS_S_COMPLETE) {

lib/gssapi/mech/gss_compare_name.c
@@ -44,7 +44,7 @@ gss_compare_name(OM_uint32 *minor_status,
 	 */
 	if (name1->gn_value.value && name2->gn_value.value) {
 		*name_equal = 1;
-		if (!gss_oid_equal(&name1->gn_type, &name2->gn_type)) {
+		if (!gss_oid_equal(name1->gn_type, name2->gn_type)) {
 			*name_equal = 0;
 		} else if (name1->gn_value.length != name2->gn_value.length ||
 		    memcmp(name1->gn_value.value, name2->gn_value.value,

lib/gssapi/mech/gss_display_name.c
@@ -62,7 +62,7 @@ gss_display_name(OM_uint32 *minor_status,
 		memcpy(output_name_buffer->value, name->gn_value.value,
 		    output_name_buffer->length);
 		if (output_name_type)
-			*output_name_type = &name->gn_type;
+			*output_name_type = name->gn_type;
 
 		*minor_status = 0;
 		return (GSS_S_COMPLETE);

lib/gssapi/mech/gss_duplicate_name.c
@@ -48,7 +48,7 @@ gss_duplicate_name(OM_uint32 *minor_status,
 	 */
 	if (name->gn_value.value) {
 		major_status = gss_import_name(minor_status,
-		    &name->gn_value, &name->gn_type, dest_name);
+		    &name->gn_value, name->gn_type, dest_name);
 		if (major_status != GSS_S_COMPLETE)
 			return (major_status);
 		new_name = (struct _gss_name *) *dest_name;

lib/gssapi/mech/gss_duplicate_oid.c
@@ -47,22 +47,5 @@ gss_duplicate_oid (
 	return GSS_S_COMPLETE;
     }
 
-    *dest_oid = malloc(sizeof(**dest_oid));
-    if (*dest_oid == GSS_C_NO_OID) {
-	*minor_status = ENOMEM;
-	return GSS_S_FAILURE;
-    }
-
-    (*dest_oid)->elements = malloc(src_oid->length);
-    if ((*dest_oid)->elements == NULL) {
-	free(*dest_oid);
-	*dest_oid = GSS_C_NO_OID;
-	*minor_status = ENOMEM;
-	return GSS_S_FAILURE;
-    }
-    memcpy((*dest_oid)->elements, src_oid->elements, src_oid->length);
-    (*dest_oid)->length = src_oid->length;
-
-    *minor_status = 0;
-    return GSS_S_COMPLETE;
+    return _gss_intern_oid(minor_status, src_oid, dest_oid);
 }

lib/gssapi/mech/gss_import_name.c
@@ -221,7 +221,7 @@ gss_import_name(OM_uint32 *minor_status,
 
 	HEIM_SLIST_INIT(&name->gn_mn);
 
-	major_status = _gss_copy_oid(minor_status,
+	major_status = _gss_intern_oid(minor_status,
 	    name_type, &name->gn_type);
 	if (major_status) {
 		free(name);
@@ -256,8 +256,7 @@ gss_import_name(OM_uint32 *minor_status,
 
 		major_status = (*m->gm_mech.gm_import_name)(minor_status,
 		    &name->gn_value,
-		    (name->gn_type.elements
-			? &name->gn_type : GSS_C_NO_OID),
+		    name->gn_type,
 		    &mn->gmn_name);
 		if (major_status != GSS_S_COMPLETE) {
 			_gss_mg_error(&m->gm_mech, major_status, *minor_status);
@@ -266,7 +265,7 @@ gss_import_name(OM_uint32 *minor_status,
 		}
 
 		mn->gmn_mech = &m->gm_mech;
-		mn->gmn_mech_oid = &m->gm_mech_oid;
+		mn->gmn_mech_oid = m->gm_mech_oid;
 		HEIM_SLIST_INSERT_HEAD(&name->gn_mn, mn, gmn_link);
 	}
 

commit 3c92747f2a632c2e1ca5c60a00fefb6d2e3cccad

gss: fix leak in add_builtin() if interning OID fails
4a93c477 (#447) introduced a leak in add_builtin().  Reported by
jaltman@auristor.com.

lib/gssapi/mech/gss_mech_switch.c
@@ -217,8 +217,10 @@ add_builtin(gssapi_mech_interface mech)
     m->gm_so = NULL;
     m->gm_mech = *mech;
     _gss_intern_oid(&minor_status, &mech->gm_mech_oid, &m->gm_mech_oid);
-    if (minor_status)
+    if (minor_status) {
+	free(m);
 	return minor_status;
+    }
     gss_add_oid_set_member(&minor_status,
 			   &m->gm_mech.gm_mech_oid, &_gss_mech_oids);
 
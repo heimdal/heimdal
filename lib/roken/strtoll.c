heimdal/heimdal

commit b10ad7eb5795f116e6ad623d0a73d57491d0f271

roken: strtoll.c negation is a no-op on unsigned integer
strtoll() returns a signed long long not an unsigned long long.
When applying the negation for negatives the value must be cast
from unsigned to signed and then stored in a signed variable
before returning it.

Change-Id: If568afd2509d27c7bf206ca59d32ca150cb34857

lib/roken/strtoll.c
@@ -57,6 +57,7 @@ strtoll(const char * nptr, char ** endptr, int base)
 {
     const char *s;
     unsigned long long acc;
+    long long ret = 0;
     char c;
     unsigned long long cutoff;
     int neg, any, cutlim;
@@ -135,15 +136,14 @@ strtoll(const char * nptr, char ** endptr, int base)
 	}
     }
     if (any < 0) {
-	acc = neg ? LLONG_MIN : LLONG_MAX;
+	ret = neg ? LLONG_MIN : LLONG_MAX;
 	errno = ERANGE;
     } else if (!any) {
 noconv:
 	errno = EINVAL;
     } else if (neg)
-	acc = -acc;
+	ret = -(long long)acc;
     if (endptr != NULL)
 	*endptr = (char *)(any ? s - 1 : nptr);
-    return (acc);
+    return ret;
 }
-

commit f0d9289d86e3f4081d096493853c182ca0efef38

roken: fix strtoll
b10ad7eb5795f116e6ad623d0a73d57491d0f271
("roken: strtoll.c negation is a no-op on unsigned integer")
broke strtoll() by failing to assign 'ret' in the success case.

Change-Id: I30535d83a2bef305140f1a6bd1ed2eeba23db9b9

lib/roken/strtoll.c
@@ -141,8 +141,11 @@ strtoll(const char * nptr, char ** endptr, int base)
     } else if (!any) {
 noconv:
 	errno = EINVAL;
-    } else if (neg)
+    } else if (neg) {
 	ret = -(long long)acc;
+    } else {
+	ret = acc;
+    }
     if (endptr != NULL)
 	*endptr = (char *)(any ? s - 1 : nptr);
     return ret;
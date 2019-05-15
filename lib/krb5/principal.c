2f110d6608a1f167052df0c1813b839ee7c2cd02
Jeffrey Altman <jaltman@secure-endpoints.com>
2019-05-15 04:02:47 -0400

    krb5_principal_is_anonymous
    _krb5_principal_is_anonymous() is used outside lib/krb5 and
    therefore it needs to be properly exported and its flag macros
    need to be in a public header: krb5.h not krb5_locl.h.
    
    Including krb5_locl.h from within kuser_locl.h for instance
    results in build failures on Solaris.
    
    This change renames the function and makes it part of the public
    api.
    
    Change-Id: I130d1698b10bdbd150b95e8c7d32dfc362889ce6
    (cherry picked from commit bdcd7d2f3dc02f902628a9145084cf7a80496f7a)

--- lib/krb5/principal.c
+++ lib/krb5/principal.c
@@ -1253,8 +1253,8 @@ krb5_principal_is_root_krbtgt(krb5_context context, krb5_const_principal p)
  * @ingroup krb5_principal
  */
 
-krb5_boolean KRB5_LIB_FUNCTION
-_krb5_principal_is_anonymous(krb5_context context,
+KRB5_LIB_FUNCTION krb5_boolean KRB5_LIB_CALL
+krb5_principal_is_anonymous(krb5_context context,
 			     krb5_const_principal p,
 			     unsigned int flags)
 {

4757a1235326840ddfe054154e1e58c10ec97de0
Viktor Dukhovni <viktor@twosigma.com>
2019-09-21 18:29:12 -0400

    Disable Nagle in iprop master and slave

--- lib/kadm5/kadm5_locl.h
+++ lib/kadm5/kadm5_locl.h
@@ -67,6 +67,12 @@
 #ifdef HAVE_SYS_SOCKET_H
 #include <sys/socket.h>
 #endif
+#ifdef HAVE_NETINET_IN_H
+#include <netinet/in.h>
+#endif
+#ifdef HAVE_NETINET_TCP_H
+#include <netinet/tcp.h>
+#endif
 #ifdef HAVE_SYS_UN_H
 #include <sys/un.h>
 #endif

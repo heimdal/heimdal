heimdal/heimdal

commit bcb5ed28fbe25d3b207d17ddd2f420f938a4ef03

kdc/kx509.c: clean up krb5_get_error_message strings used for mk_error_resp
mk_error_response expects the strings it builds will need to be freed but
not those passed in; krb5_get_error_message generates a string which needs to
be freed. keep track of and free those strings.

kdc/kx509.c
@@ -892,10 +892,12 @@ _kdc_do_kx509(kx509_req_context r)
     /* Authenticate the request (consume the AP-REQ) */
     ret = krb5_kt_resolve(r->context, "HDBGET:", &id);
     if (ret) {
+	const char *msg = krb5_get_error_message(r->context, ret);
         ret = mk_error_response(r->context, r, 1,
                                 KRB5KDC_ERR_S_PRINCIPAL_UNKNOWN,
                                 "Can't open HDB/keytab for kx509: %s",
-                                krb5_get_error_message(r->context, ret));
+				msg);
+	krb5_free_error_message(context, msg);
         goto out;
     }
 
@@ -972,18 +974,20 @@ _kdc_do_kx509(kx509_req_context r)
     /* Extract and parse CSR or a DER-encoded RSA public key */
     ret = get_csr(r->context, r);
     if (ret) {
+	const char *msg = krb5_get_error_message(r->context, ret);
         ret = mk_error_response(r->context, r, 3, ret,
-                                "Failed to parse CSR: %s",
-                                krb5_get_error_message(r->context, ret));
+                                "Failed to parse CSR: %s", msg);
+	krb5_free_error_message(context, msg);
         goto out;
     }
 
     /* Authorize the request */
     ret = check_authz(r->context, r, cprincipal);
     if (ret) {
+	const char *msg = krb5_get_error_message(r->context, ret);
         ret = mk_error_response(r->context, r, 3, ret,
-                                "Rejected by policy: %s",
-                                krb5_get_error_message(r->context, ret));
+                                "Rejected by policy: %s", msg);
+	krb5_free_error_message(context, msg);
         goto out;
     }
 
@@ -1003,20 +1007,23 @@ _kdc_do_kx509(kx509_req_context r)
                                 &r->ticket_times, r->send_chain, &certs);
     if (ret) {
         int level = 1;
+	const char *msg = krb5_get_error_message(r->context, ret);
 
         if (ret == KRB5KDC_ERR_POLICY)
             level = 4; /* _kdc_audit_trail() logs at level 3 */
         ret = mk_error_response(r->context, r, level, ret,
-                                "Certificate isuance failed: %s",
-                                krb5_get_error_message(r->context, ret));
+                                "Certificate isuance failed: %s", msg);
+	krb5_free_error_message(context, msg);
         goto out;
     }
 
     ret = encode_cert_and_chain(r->context->hx509ctx, certs, rep.certificate);
     if (ret) {
+	const char *msg = krb5_get_error_message(r->context, ret);
         ret = mk_error_response(r->context, r, 1, ret,
                                 "Could not encode certificate and chain: %s",
-                                krb5_get_error_message(r->context, ret));
+				msg);
+	krb5_free_error_message(context, msg);
         goto out;
     }
 

commit 99a71b85c87b27123da193f3642ec93ea34d479d

kdc: kx509.c fix build failures
bcb5ed28fbe25d3b207d17ddd2f420f938a4ef03 ("kdc/kx509.c: clean up
krb5_get_error_message strings used for mk_error_resp") introduced a
build failure.  'context' is not a local variable but a field in the
kx509_req_context.

Change-Id: I38a8a9b1a19c3370dfc039d6e09445425936c32b

kdc/kx509.c
@@ -897,7 +897,7 @@ _kdc_do_kx509(kx509_req_context r)
                                 KRB5KDC_ERR_S_PRINCIPAL_UNKNOWN,
                                 "Can't open HDB/keytab for kx509: %s",
 				msg);
-	krb5_free_error_message(context, msg);
+	krb5_free_error_message(r->context, msg);
         goto out;
     }
 
@@ -977,7 +977,7 @@ _kdc_do_kx509(kx509_req_context r)
 	const char *msg = krb5_get_error_message(r->context, ret);
         ret = mk_error_response(r->context, r, 3, ret,
                                 "Failed to parse CSR: %s", msg);
-	krb5_free_error_message(context, msg);
+	krb5_free_error_message(r->context, msg);
         goto out;
     }
 
@@ -987,7 +987,7 @@ _kdc_do_kx509(kx509_req_context r)
 	const char *msg = krb5_get_error_message(r->context, ret);
         ret = mk_error_response(r->context, r, 3, ret,
                                 "Rejected by policy: %s", msg);
-	krb5_free_error_message(context, msg);
+	krb5_free_error_message(r->context, msg);
         goto out;
     }
 
@@ -1013,7 +1013,7 @@ _kdc_do_kx509(kx509_req_context r)
             level = 4; /* _kdc_audit_trail() logs at level 3 */
         ret = mk_error_response(r->context, r, level, ret,
                                 "Certificate isuance failed: %s", msg);
-	krb5_free_error_message(context, msg);
+	krb5_free_error_message(r->context, msg);
         goto out;
     }
 
@@ -1023,7 +1023,7 @@ _kdc_do_kx509(kx509_req_context r)
         ret = mk_error_response(r->context, r, 1, ret,
                                 "Could not encode certificate and chain: %s",
 				msg);
-	krb5_free_error_message(context, msg);
+	krb5_free_error_message(r->context, msg);
         goto out;
     }
 
bf3c4219fe01c50edbc19361592b6846d132e306
Andrew Bartlett <abartlet@samba.org>
2020-05-28 20:09:43 +1200

    Make rk_SOCK_INIT an inline function and check the result to avoid warnings
    Similar to f6e0d19cc036bc3601c2b8a8d9c064adbace9737 but
    fixed in the header by making it a proper static inline
    function (as some callers treats it as one, so do it
    for all now for consistency).
    
    Seen on Ubuntu 18.04 with
    
    giving:
    
    In file included from getaddrinfo-test.c:36:0:
    getaddrinfo-test.c: In function ‘main’:
    roken.h:110:24: error: statement with no effect [-Werror=unused-value]
     #define rk_SOCK_INIT() 0
                            ^
    getaddrinfo-test.c:132:5: note: in expansion of macro ‘rk_SOCK_INIT’
         rk_SOCK_INIT();
         ^~~~~~~~~~~~
    
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>

--- lib/roken/getaddrinfo-test.c
+++ lib/roken/getaddrinfo-test.c
@@ -129,7 +129,8 @@ main(int argc, char **argv)
 	return 0;
     }
 
-    rk_SOCK_INIT();
+    if (rk_SOCK_INIT())
+	errx(1, "Couldn't initialize sockets. Err=%d\n", rk_SOCK_ERRNO);
 
     argc -= optidx;
     argv += optidx;

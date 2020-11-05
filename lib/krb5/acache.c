heimdal/heimdal

commit 7bf4d76e75e904dd65a0fbb90c9cad981245f714

krb5: Improve cccol sub naming; add gss_store_cred_into2()
 - Formalize the TYPE:collection_name:subsidiary_name naming scheme for
   ccaches in ccache collections
    - KEYRING: ccaches are weird because they have one more optional field: the
      "anchor", so rather than just assume a naming convention everywhere, we
      add new functions as well
 - Add krb5_cc_{resolve,default}_sub() that allows one to specify a
   "subsidiary" ccache name in a collection separately from the
   collection name
 - Add krb5_cc_{resolve,default}_for() which take a principal name,
   unparse it, and use it as the subsidiary ccache name (with colons
   replaced)
 - Make kinit use the new interfaces
 - Add missing DIR ccache iteration functionality
 - Revamps test_cc
 - Add krb5_cc_get_collection() and krb5_cc_get_subsidiary()
 - Bump the ccops SPI version number
 - Add gss_store_cred_into2()
 - Make MEMORY:anonymous not linked into the global MEMORY ccache
   collection, and uses this for delegated cred handles

TBD:

 - Split this up into a krb5 change and gss mech_krb5 change?
 - Add krb5_cc_init_and_store() utility, per Greg's suggestion?

lib/krb5/acache.c
@@ -49,6 +49,7 @@ static void *cc_handle;
 
 typedef struct krb5_acc {
     char *cache_name;
+    char *cache_subsidiary;
     cc_context_t context;
     cc_ccache_t ccache;
 } krb5_acc;
@@ -442,41 +443,51 @@ get_cc_name(krb5_acc *a)
 }
 
 
-static const char* KRB5_CALLCONV
+static krb5_error_code KRB5_CALLCONV
 acc_get_name(krb5_context context,
-	     krb5_ccache id)
+	     krb5_ccache id,
+             const char **name,
+             const char **colname,
+             const char **subsidiary)
 {
+    krb5_error_code ret;
     krb5_acc *a = ACACHE(id);
     int32_t error;
 
-    if (a->cache_name == NULL) {
-	krb5_error_code ret;
-	krb5_principal principal;
-	char *name;
+    if (name)
+        *name = NULL;
+    if (colname)
+        *colname = NULL;
+    if (subsidiary)
+        *subsidiary = NULL;
+    if (a->cache_subsidiary == NULL) {
+	krb5_principal principal = NULL;
 
 	ret = _krb5_get_default_principal_local(context, &principal);
-	if (ret)
-	    return NULL;
-
-	ret = krb5_unparse_name(context, principal, &name);
+	if (ret == 0)
+            ret = krb5_unparse_name(context, principal, &a->cache_subsidiary);
 	krb5_free_principal(context, principal);
 	if (ret)
-	    return NULL;
-
-	error = (*a->context->func->create_new_ccache)(a->context,
-						       cc_credentials_v5,
-						       name,
-						       &a->ccache);
-	krb5_xfree(name);
-	if (error)
-	    return NULL;
-
-	error = get_cc_name(a);
-	if (error)
-	    return NULL;
+	    return ret;
     }
 
-    return a->cache_name;
+    if (a->cache_name == NULL) {
+        error = (*a->context->func->create_new_ccache)(a->context,
+                                                       cc_credentials_v5,
+                                                       a->cache_subsidiary,
+                                                       &a->ccache);
+        if (error == ccNoError)
+            error = get_cc_name(a);
+        if (error != ccNoError)
+            ret = translate_cc_error(context, error);
+    }
+    if (name)
+        *name = a->cache_name;
+    if (colname)
+        *colname = "";
+    if (subsidiary)
+    *subsidiary = a->cache_subsidiary;
+    return ret;
 }
 
 static krb5_error_code KRB5_CALLCONV
@@ -497,6 +508,10 @@ acc_alloc(krb5_context context, krb5_ccache *id)
     }
 
     a = ACACHE(*id);
+    a->cache_subsidiary = NULL;
+    a->cache_name = NULL;
+    a->context = NULL;
+    a->ccache = NULL;
 
     error = (*init_func)(&a->context, ccapi_version_3, NULL, NULL);
     if (error) {
@@ -504,17 +519,17 @@ acc_alloc(krb5_context context, krb5_ccache *id)
 	return translate_cc_error(context, error);
     }
 
-    a->cache_name = NULL;
-
     return 0;
 }
 
 static krb5_error_code KRB5_CALLCONV
-acc_resolve(krb5_context context, krb5_ccache *id, const char *res)
+acc_resolve(krb5_context context, krb5_ccache *id, const char *res, const char *sub)
 {
     krb5_error_code ret;
+    cc_time_t offset;
     cc_int32 error;
     krb5_acc *a;
+    char *s = NULL;
 
     ret = acc_alloc(context, id);
     if (ret)
@@ -522,49 +537,44 @@ acc_resolve(krb5_context context, krb5_ccache *id, const char *res)
 
     a = ACACHE(*id);
 
-    error = (*a->context->func->open_ccache)(a->context, res, &a->ccache);
-    if (error == ccNoError) {
-	cc_time_t offset;
-	error = get_cc_name(a);
-	if (error != ccNoError) {
+    if (sub) {
+        if (asprintf(&s, "%s:%s", res, sub) == -1 || s == NULL ||
+            (a->cache_subsidiary = strdup(sub)) == NULL) {
 	    acc_close(context, *id);
-	    *id = NULL;
-	    return translate_cc_error(context, error);
-	}
-
-	error = (*a->ccache->func->get_kdc_time_offset)(a->ccache,
-							cc_credentials_v5,
-							&offset);
-	if (error == 0)
-	    context->kdc_sec_offset = offset;
-
-    } else if (error == ccErrCCacheNotFound) {
-	a->ccache = NULL;
-	a->cache_name = NULL;
-    } else {
-	*id = NULL;
-	return translate_cc_error(context, error);
+            free(s);
+            return krb5_enomem(context);
+        }
+        res = s;
+        /*
+         * XXX With a bit of extra refactoring we could use the collection name
+         * as the path to the shared object implementing CCAPI...  For now we
+         * ignore the collection name.
+         */
     }
 
+    error = (*a->context->func->open_ccache)(a->context, res, &a->ccache);
+    if (error == ccNoError)
+        error = get_cc_name(a);
+    if (error != ccNoError) {
+        acc_close(context, *id);
+        *id = NULL;
+        free(s);
+        return translate_cc_error(context, error);
+    }
+
+    error = (*a->ccache->func->get_kdc_time_offset)(a->ccache,
+                                                    cc_credentials_v5,
+                                                    &offset);
+    if (error == 0)
+        context->kdc_sec_offset = offset;
+    free(s);
     return 0;
 }
 
 static krb5_error_code KRB5_CALLCONV
 acc_gen_new(krb5_context context, krb5_ccache *id)
 {
-    krb5_error_code ret;
-    krb5_acc *a;
-
-    ret = acc_alloc(context, id);
-    if (ret)
-	return ret;
-
-    a = ACACHE(*id);
-
-    a->ccache = NULL;
-    a->cache_name = NULL;
-
-    return 0;
+    return acc_alloc(context, id);
 }
 
 static krb5_error_code KRB5_CALLCONV

kcm/glue.c
@@ -44,15 +44,27 @@ RCSID("$Id$");
 #define KCMCACHE(X)	((kcm_ccache)(X)->data.data)
 #define CACHENAME(X)	(KCMCACHE(X)->name)
 
-static const char *
+static krb5_error_code
 kcmss_get_name(krb5_context context,
-	       krb5_ccache id)
+	       krb5_ccache id,
+               const char **name,
+               const char **col,
+               const char **sub)
 {
-    return CACHENAME(id);
+    if (name)
+        *name = CACHENAME(id);
+    if (col)
+        *col = NULL;
+    if (name)
+        *sub = CACHENAME(id);
+    return 0;
 }
 
 static krb5_error_code
-kcmss_resolve(krb5_context context, krb5_ccache *id, const char *res)
+kcmss_resolve(krb5_context context,
+              krb5_ccache *id,
+              const char *res,
+              const char *sub)
 {
     return KRB5_FCC_INTERNAL;
 }

kuser/kinit.c
@@ -1316,65 +1316,12 @@ get_princ_kt(krb5_context context,
     free(def_realm);
 }
 
-static krb5_error_code
-get_switched_ccache(krb5_context context,
-		    const char * type,
-		    krb5_principal principal,
-		    krb5_ccache *ccache)
-{
-    krb5_error_code ret;
-
-#ifdef _WIN32
-    if (strcmp(type, "API") == 0) {
-	/*
-	 * Windows stores the default ccache name in the
-	 * registry which is shared across multiple logon
-	 * sessions for the same user.  The API credential
-	 * cache provides a unique name space per logon
-	 * session.  Therefore there is no need to generate
-	 * a unique ccache name.  Instead use the principal
-	 * name.  This provides a friendlier user experience.
-	 */
-	char * unparsed_name;
-	char * cred_cache;
-
-	ret = krb5_unparse_name(context, principal,
-				&unparsed_name);
-	if (ret)
-	    krb5_err(context, 1, ret,
-		     N_("unparsing principal name", ""));
-
-	ret = asprintf(&cred_cache, "API:%s", unparsed_name);
-	krb5_free_unparsed_name(context, unparsed_name);
-	if (ret == -1 || cred_cache == NULL)
-	    krb5_err(context, 1, ret,
-		      N_("building credential cache name", ""));
-
-	ret = krb5_cc_resolve(context, cred_cache, ccache);
-	free(cred_cache);
-    } else if (strcmp(type, "MSLSA") == 0) {
-	/*
-	 * The Windows MSLSA cache when it is writeable
-	 * stores tickets for multiple client principals
-	 * in a single credential cache.
-	 */
-	ret = krb5_cc_resolve(context, "MSLSA:", ccache);
-    } else {
-	ret = krb5_cc_new_unique(context, type, NULL, ccache);
-    }
-#else /* !_WIN32 */
-    ret = krb5_cc_new_unique(context, type, NULL, ccache);
-#endif /* _WIN32 */
-
-    return ret;
-}
-
 int
 main(int argc, char **argv)
 {
     krb5_error_code ret;
     krb5_context context;
-    krb5_ccache  ccache;
+    krb5_ccache  ccache = NULL;
     krb5_principal principal = NULL;
     int optidx = 0;
     krb5_deltat ticket_life = 0;
@@ -1477,42 +1424,8 @@ main(int argc, char **argv)
 
     if (cred_cache)
 	ret = krb5_cc_resolve(context, cred_cache, &ccache);
-    else {
-	if (argc > 1) {
-	    char s[1024];
-	    ret = krb5_cc_new_unique(context, NULL, NULL, &ccache);
-	    if (ret)
-		krb5_err(context, 1, ret, "creating cred cache");
-	    snprintf(s, sizeof(s), "%s:%s",
-		     krb5_cc_get_type(context, ccache),
-		     krb5_cc_get_name(context, ccache));
-	    setenv("KRB5CCNAME", s, 1);
-	    unique_ccache = TRUE;
-	} else {
-	    ret = krb5_cc_cache_match(context, principal, &ccache);
-	    if (ret) {
-		const char *type;
-		ret = krb5_cc_default(context, &ccache);
-		if (ret)
-		    krb5_err(context, 1, ret,
-			     N_("resolving credentials cache", ""));
-
-		/*
-		 * Check if the type support switching, and we do,
-		 * then do that instead over overwriting the current
-		 * default credential
-		 */
-		type = krb5_cc_get_type(context, ccache);
-		if (krb5_cc_support_switch(context, type)) {
-		    krb5_cc_close(context, ccache);
-		    ret = get_switched_ccache(context, type, principal,
-					      &ccache);
-		    if (ret == 0)
-			unique_ccache = TRUE;
-		}
-	    }
-	}
-    }
+    else
+        ret = krb5_cc_default_for(context, principal, &ccache);
     if (ret)
 	krb5_err(context, 1, ret, N_("resolving credentials cache", ""));
 

lib/gssapi/gssapi/gssapi.h
@@ -1187,6 +1187,29 @@ gss_store_cred_into(
     gss_cred_usage_t * /* cred_usage_stored */
     );
 
+GSSAPI_LIB_FUNCTION OM_uint32 GSSAPI_LIB_CALL
+gss_store_cred_into2(
+    OM_uint32 * /* minor_status */,
+    gss_const_cred_id_t /* input_cred_handle */,
+    gss_cred_usage_t /* input_usage */,
+    const gss_OID /* desired_mech */,
+    OM_uint32 /* store_cred_flags */,
+    gss_const_key_value_set_t /* cred_store */,
+    gss_OID_set * /* elements_stored */,
+    gss_cred_usage_t * /* cred_usage_stored */,
+    gss_buffer_set_t * /* env */
+    );
+
+enum gss_store_cred_flags {
+    GSS_C_STORE_CRED_DEFAULT = 1,
+    GSS_C_STORE_CRED_OVERWRITE = 2,
+    GSS_C_STORE_CRED_SET_PROCESS = 4,
+};
+#define GSS_C_STORE_CRED_DEFAULT GSS_C_STORE_CRED_DEFAULT
+#define GSS_C_STORE_CRED_OVERWRITE GSS_C_STORE_CRED_OVERWRITE
+#define GSS_C_STORE_CRED_SET_PROCESS GSS_C_STORE_CRED_SET_PROCESS
+
+
 GSSAPI_LIB_FUNCTION OM_uint32 GSSAPI_CALLCONV
 gss_set_neg_mechs(
     OM_uint32 * /* minor_status */,

lib/gssapi/gssapi_mech.h
@@ -475,6 +475,17 @@ _gss_store_cred_into_t(OM_uint32 *minor_status,
 		       gss_cred_usage_t *cred_usage_stored);
 
 typedef OM_uint32 GSSAPI_CALLCONV
+_gss_store_cred_into2_t(OM_uint32 *minor_status,
+		        gss_const_cred_id_t input_cred_handle,
+		        gss_cred_usage_t input_usage,
+		        gss_OID desired_mech,
+		        OM_uint32 store_cred_flags,
+		        gss_const_key_value_set_t cred_store,
+		        gss_OID_set *elements_stored,
+		        gss_cred_usage_t *cred_usage_stored,
+                        gss_buffer_set_t *env);
+
+typedef OM_uint32 GSSAPI_CALLCONV
 _gss_set_neg_mechs_t(OM_uint32 *minor_status,
 		     gss_cred_id_t cred_handle,
 		     const gss_OID_set mechs);
@@ -623,6 +634,7 @@ typedef struct gssapi_mech_interface_desc {
 	_gss_query_mechanism_info_t	*gm_query_mechanism_info;
 	_gss_query_meta_data_t		*gm_query_meta_data;
 	_gss_exchange_meta_data_t	*gm_exchange_meta_data;
+	_gss_store_cred_into2_t		*gm_store_cred_into2;
         struct gss_mech_compat_desc_struct  *gm_compat;
 } gssapi_mech_interface_desc, *gssapi_mech_interface;
 

lib/gssapi/krb5/accept_sec_context.c
@@ -169,8 +169,7 @@ gsskrb5_accept_delegated_token
     }
 
     *delegated_cred_handle = NULL;
-    kret = krb5_cc_new_unique (context, krb5_cc_type_memory,
-                               NULL, &ccache);
+    kret = krb5_cc_resolve(context, "MEMORY:anonymous", &ccache);
     if (kret) {
 	ctx->flags &= ~GSS_C_DELEG_FLAG;
 	goto out;
@@ -204,7 +203,7 @@ gsskrb5_accept_delegated_token
 	gsskrb5_cred handle;
 
 	ret = _gsskrb5_krb5_import_cred(minor_status,
-					ccache,
+					&ccache,
 					NULL,
 					NULL,
 					delegated_cred_handle);
@@ -212,10 +211,7 @@ gsskrb5_accept_delegated_token
 	    goto out;
 
 	handle = (gsskrb5_cred) *delegated_cred_handle;
-
 	handle->cred_flags |= GSS_CF_DESTROY_CRED_ON_RELEASE;
-	krb5_cc_close(context, ccache);
-	ccache = NULL;
     }
 
 out:

lib/gssapi/krb5/copy_ccache.c
@@ -62,9 +62,18 @@ gss_krb5_copy_ccache(OM_uint32 *minor_status,
 #endif
 
 
+/*
+ * WARNING: Takes ownership of `id'.  Because MEMORY:anonymous is now not
+ * linked into to the MEMORY ccache namespace, we can't use krb5_cc_resolve()
+ * with the cache's name anymore.  We need a krb5_cc_clone() or some such, with
+ * attendant new method for ccops.  Or we could create a new MEMORY:anonymous
+ * ccache and copy all the creds from `id' into it.  But we know callers of
+ * this function don't need `id' after calling it, so for now we'll just take
+ * ownershipd of it.
+ */
 OM_uint32
 _gsskrb5_krb5_import_cred(OM_uint32 *minor_status,
-			  krb5_ccache id,
+			  krb5_ccache *id,
 			  krb5_principal keytab_principal,
 			  krb5_keytab keytab,
 			  gss_cred_id_t *cred)
@@ -88,14 +97,13 @@ _gsskrb5_krb5_import_cred(OM_uint32 *minor_status,
 
     handle->usage = 0;
 
-    if (id) {
+    if (*id) {
 	time_t now;
 	OM_uint32 left;
-	char *str;
 
 	handle->usage |= GSS_C_INITIATE;
 
-	kret = krb5_cc_get_principal(context, id,
+	kret = krb5_cc_get_principal(context, *id,
 				     &handle->principal);
 	if (kret) {
 	    free(handle);
@@ -121,7 +129,7 @@ _gsskrb5_krb5_import_cred(OM_uint32 *minor_status,
 	krb5_timeofday(context, &now);
 	ret = __gsskrb5_ccache_lifetime(minor_status,
 					context,
-					id,
+					*id,
 					handle->principal,
 					&left);
 	if (ret != GSS_S_COMPLETE) {
@@ -131,12 +139,8 @@ _gsskrb5_krb5_import_cred(OM_uint32 *minor_status,
 	}
 	handle->endtime = now + left;
 
-	kret = krb5_cc_get_full_name(context, id, &str);
-	if (kret)
-	    goto out;
-
-	kret = krb5_cc_resolve(context, str, &handle->ccache);
-	free(str);
+        handle->ccache = *id;
+        *id = NULL;
 	if (kret)
 	    goto out;
     }

lib/gssapi/krb5/external.c
@@ -404,6 +404,7 @@ static gssapi_mech_interface_desc krb5_mech = {
     NULL, /* gm_query_mechanism_info */
     NULL, /* gm_query_meta_data */
     NULL, /* gm_exchange_meta_data */
+    _gsskrb5_store_cred_into2,
     NULL  /* gm_compat */
 };
 

commit 3bc006bfb9fafcb8c53678d394865615d1e5b65f

lib/krb5: acc_resolve_2 leak if ccErrCacheNotFound
Fix the possible leak of 's' allocated by asprintf()

When squashing the changes reference "this fixes an issue introduced by 7bf4d76 ("krb5: Improve cccol sub naming; add gss_store_cred_into2()")" in the commit message.

lib/krb5/acache.c
@@ -560,7 +560,8 @@ acc_resolve_2(krb5_context context, krb5_ccache *id, const char *res, const char
     if (error == ccErrCCacheNotFound) {
         a->ccache = NULL;
         a->cache_name = NULL;
-        return 0;
+	free(s);
+	return 0;
     }
     if (error == ccNoError)
         error = get_cc_name(a);
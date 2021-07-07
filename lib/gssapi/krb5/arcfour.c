heimdal/heimdal

commit 4b543b727a3a692e5b1d52620ba740944d565046

gssapi/krb5: fix rc4-hmac gss_unwrap_iov() without DCE_STYLE
gss_unwrap_iov() with rc4-hmac (RFC4757) encryption types would fail unless
GSS_C_DCE_STYLE was specified, as an incorrect length was passed to
_gssapi_verify_mech_header(). (The correct length is the header length for
GSS_C_DCE_STYLE, and the wrap token length otherwise.)

lib/gssapi/krb5/arcfour.c
@@ -1197,15 +1197,19 @@ _gssapi_unwrap_iov_arcfour(OM_uint32 *minor_status,
 	return GSS_S_FAILURE;
     }
 
-    if (IS_DCE_STYLE(context)) {
-	verify_len = GSS_ARCFOUR_WRAP_TOKEN_SIZE +
-		     GSS_ARCFOUR_WRAP_TOKEN_DCE_DER_HEADER_SIZE;
-	if (header->buffer.length > verify_len) {
-	    return GSS_S_BAD_MECH;
+    verify_len = header->buffer.length;
+
+    if (!IS_DCE_STYLE(context)) {
+	for (i = 0; i < iov_count; i++) {
+	    /* length in header also includes data and padding */
+	    if (GSS_IOV_BUFFER_TYPE(iov[i].type) == GSS_IOV_BUFFER_TYPE_DATA)
+		verify_len += iov[i].buffer.length;
 	}
-    } else {
-	verify_len = header->buffer.length;
+
+	if (padding)
+	    verify_len += padding->buffer.length;
     }
+
     _p = header->buffer.value;
 
     ret = _gssapi_verify_mech_header(&_p,

commit 0dd19003082f50b729328a4161b4462d6e06ed05

gss: fix regression in rc4-hmac krb5 DCE unwrap
4b543b7 introduced a regression in the krb5 mechanism's gss_unwrap for
DCE applications, owing to IS_DCE_STYLE() being called with a krb5
instead of mechanism context handle.

lib/gssapi/krb5/arcfour.c
@@ -1199,7 +1199,7 @@ _gssapi_unwrap_iov_arcfour(OM_uint32 *minor_status,
 
     verify_len = header->buffer.length;
 
-    if (!IS_DCE_STYLE(context)) {
+    if (!IS_DCE_STYLE(ctx)) {
 	for (i = 0; i < iov_count; i++) {
 	    /* length in header also includes data and padding */
 	    if (GSS_IOV_BUFFER_TYPE(iov[i].type) == GSS_IOV_BUFFER_TYPE_DATA)
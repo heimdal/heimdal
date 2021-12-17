heimdal/heimdal

commit 378f34b4be9865ed3949918fba8d2dd877b395c0

Always to CANON for tgs

kdc/krb5tgs.c
@@ -1482,7 +1482,7 @@ tgs_build_reply(krb5_context context,
 		const struct sockaddr *from_addr)
 {
     krb5_error_code ret;
-    krb5_principal cp = NULL, sp = NULL, tp = NULL, dp = NULL;
+    krb5_principal cp = NULL, sp = NULL, rsp = NULL, tp = NULL, dp = NULL;
     krb5_principal krbtgt_principal = NULL;
     char *spn = NULL, *cpn = NULL, *tpn = NULL, *dpn = NULL;
     hdb_entry_ex *server = NULL, *client = NULL, *s4u2self_impersonated_client = NULL;
@@ -1518,8 +1518,10 @@ tgs_build_reply(krb5_context context,
     s = b->sname;
     r = b->realm;
 
-    if (b->kdc_options.canonicalize)
-	flags |= HDB_F_CANON;
+    /* 
+     * Always to do CANON, see comment below about returned server principal (rsp).
+     */
+    flags |= HDB_F_CANON;
 
     if(b->kdc_options.enc_tkt_in_skey){
 	Ticket *t;
@@ -1658,6 +1660,18 @@ server_lookup:
 	    ret = KRB5KDC_ERR_S_PRINCIPAL_UNKNOWN;
 	goto out;
     }
+
+    /* the name returned to the client depend on what was asked for,
+     * return canonical name if kdc_options.canonicalize was set, the
+     * client wants the true name of the principal, if not it just
+     * wants the name its asked for.
+     */
+
+    if (b->kdc_options.canonicalize)
+	rsp = server->entry.principal;
+    else
+	rsp = sp;
+
 
     /*
      * Select enctype, return key and kvno.
@@ -2208,7 +2222,7 @@ server_lookup:
 			 kvno,
 			 *auth_data,
 			 server,
-			 server->entry.principal,
+			 rsp,
 			 spn,
 			 client,
 			 cp,

commit faed69ede88f28eb12371f462cd83141613cbe8d

tgs-rep: always return canonical realm when force_canonicalize set
This allows Samba to have a behaviour where
even if canonicalize flag is not set, the canonical realm
is returned, allowing a HDB module to require behaviour
that is the same as Windows.

Regression (for a HDB module wanting AD behaviour) was
introduced by commit:
378f34b4be9865ed3949918fba8d2dd877b395c0

Signed-off-by: Isaac Boukris <iboukris@gmail.com>

[abartlet@samba.org Similar to Samba commit a9e6119ca0c2a78ef314c3162122539ee834aa04
 but made conditional on server->entry.flags.force_canonicalize to
 allow upstream submission]

kdc/krb5tgs.c
@@ -611,7 +611,11 @@ tgs_make_reply(astgs_request_t r,
     if(ret)
 	goto out;
 
-    ret = copy_Realm(&server_principal->realm, &rep.ticket.realm);
+    if (server->entry.flags.force_canonicalize)
+	ret = copy_Realm(&server->entry.principal->realm, &rep.ticket.realm);
+    else
+	ret = copy_Realm(&server_principal->realm, &rep.ticket.realm);
+
     if (ret)
 	goto out;
     _krb5_principal2principalname(&rep.ticket.sname, server_principal);
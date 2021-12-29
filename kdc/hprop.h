0492729ba70b9f26fd90dc94137ca432f89f8bdd
Luke Howard <lukeh@padl.com>
2021-12-29 21:19:52 +1100

    kdc: remove unused v4 hprop code

--- kdc/hprop.h
+++ kdc/hprop.h
@@ -56,20 +56,4 @@ struct prop_data{
 krb5_error_code v5_prop(krb5_context, HDB*, hdb_entry_ex*, void*);
 int mit_prop_dump(void*, const char*);
 
-struct v4_principal {
-    char name[64];
-    char instance[64];
-    DES_cblock key;
-    int kvno;
-    int mkvno;
-    time_t exp_date;
-    time_t mod_date;
-    char mod_name[64];
-    char mod_instance[64];
-    int max_life;
-};
-
-int v4_prop(void*, struct v4_principal*);
-int v4_prop_dump(void *arg, const char*);
-
 #endif /* __HPROP_H__ */

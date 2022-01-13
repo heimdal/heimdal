86c2dbbe30590a6a3d25fb20737aca2f80c8815a
Luke Howard <lukeh@padl.com>
2022-01-13 14:08:41 +1100

    kdc: make most of krb5_kdc_configuration private

--- kdc/kdc.h
+++ kdc/kdc.h
@@ -56,77 +56,21 @@ enum krb5_kdc_trpolicy {
     TRPOLICY_ALWAYS_HONOUR_REQUEST
 };
 
-typedef struct krb5_kdc_configuration {
-    krb5_boolean require_preauth; /* require preauth for all principals */
-    time_t kdc_warn_pwexpire; /* time before expiration to print a warning */
+#define KRB5_KDC_CONFIGURATION_COMMON_ELEMENTS			\
+    struct HDB **db;						\
+    int num_db;							\
+    krb5_log_facility *logf;					\
+    const char *app
 
-    struct HDB **db;
-    int num_db;
+#ifndef __KDC_LOCL_H__
+struct krb5_kdc_configuration {
+    KRB5_KDC_CONFIGURATION_COMMON_ELEMENTS;
+};
+#else
+struct krb5_kdc_configuration;
+#endif
 
-    int num_kdc_processes;
-
-    krb5_boolean encode_as_rep_as_tgs_rep; /* bug compatibility */
-
-    /*
-     * Windows 2019 (and earlier versions) always sends the salt
-     * and Samba has testsuites that check this behaviour, so a
-     * Samba AD DC will set this flag to match the AS-REP packet
-     * exactly.
-     */
-    krb5_boolean force_include_pa_etype_salt;
-
-    krb5_boolean tgt_use_strongest_session_key;
-    krb5_boolean preauth_use_strongest_session_key;
-    krb5_boolean svc_use_strongest_session_key;
-    krb5_boolean use_strongest_server_key;
-
-    krb5_boolean check_ticket_addresses;
-    krb5_boolean warn_ticket_addresses;
-    krb5_boolean allow_null_ticket_addresses;
-    krb5_boolean allow_anonymous;
-    krb5_boolean historical_anon_realm;
-    krb5_boolean strict_nametypes;
-    enum krb5_kdc_trpolicy trpolicy;
-
-    krb5_boolean require_pac;
-    krb5_boolean enable_armored_pa_enc_timestamp;
-    krb5_boolean enable_unarmored_pa_enc_timestamp;
-
-    krb5_boolean enable_pkinit;
-    krb5_boolean pkinit_princ_in_cert;
-    const char *pkinit_kdc_identity;
-    const char *pkinit_kdc_anchors;
-    const char *pkinit_kdc_friendly_name;
-    const char *pkinit_kdc_ocsp_file;
-    char **pkinit_kdc_cert_pool;
-    char **pkinit_kdc_revoke;
-    int pkinit_dh_min_bits;
-    /* XXX Turn these into bit-fields */
-    int pkinit_require_binding;
-    int pkinit_allow_proxy_certs;
-    int synthetic_clients;
-    int pkinit_max_life_from_cert_extension;
-    krb5_timestamp pkinit_max_life_from_cert;
-    krb5_timestamp pkinit_max_life_bound;
-    krb5_timestamp synthetic_clients_max_life;
-    krb5_timestamp synthetic_clients_max_renew;
-
-    krb5_log_facility *logf;
-
-    int enable_digest;
-    int digests_allowed;
-
-    int enable_gss_preauth;
-    int enable_gss_auth_data;
-    gss_OID_set gss_mechanisms_allowed;
-    gss_OID_set gss_cross_realm_mechanisms_allowed;
-
-    size_t max_datagram_reply_length;
-
-    int enable_kx509;
-
-    const char *app;
-} krb5_kdc_configuration;
+typedef struct krb5_kdc_configuration krb5_kdc_configuration;
 
 #define ASTGS_REQUEST_DESC_COMMON_ELEMENTS			\
     HEIM_SVC_REQUEST_DESC_COMMON_ELEMENTS;			\
--- kdc/kdc_locl.h
+++ kdc/kdc_locl.h
@@ -66,6 +66,71 @@ struct kdc_request_desc {
 
 struct kdc_patypes;
 
+struct krb5_kdc_configuration {
+    KRB5_KDC_CONFIGURATION_COMMON_ELEMENTS;
+
+    krb5_boolean require_preauth; /* require preauth for all principals */
+    time_t kdc_warn_pwexpire; /* time before expiration to print a warning */
+    int num_kdc_processes;
+    krb5_boolean encode_as_rep_as_tgs_rep; /* bug compatibility */
+
+    /*
+     * Windows 2019 (and earlier versions) always sends the salt
+     * and Samba has testsuites that check this behaviour, so a
+     * Samba AD DC will set this flag to match the AS-REP packet
+     * exactly.
+     */
+    krb5_boolean force_include_pa_etype_salt;
+
+    krb5_boolean tgt_use_strongest_session_key;
+    krb5_boolean preauth_use_strongest_session_key;
+    krb5_boolean svc_use_strongest_session_key;
+    krb5_boolean use_strongest_server_key;
+
+    krb5_boolean check_ticket_addresses;
+    krb5_boolean warn_ticket_addresses;
+    krb5_boolean allow_null_ticket_addresses;
+    krb5_boolean allow_anonymous;
+    krb5_boolean historical_anon_realm;
+    krb5_boolean strict_nametypes;
+    enum krb5_kdc_trpolicy trpolicy;
+
+    krb5_boolean require_pac;
+    krb5_boolean enable_armored_pa_enc_timestamp;
+    krb5_boolean enable_unarmored_pa_enc_timestamp;
+
+    krb5_boolean enable_pkinit;
+    krb5_boolean pkinit_princ_in_cert;
+    const char *pkinit_kdc_identity;
+    const char *pkinit_kdc_anchors;
+    const char *pkinit_kdc_friendly_name;
+    const char *pkinit_kdc_ocsp_file;
+    char **pkinit_kdc_cert_pool;
+    char **pkinit_kdc_revoke;
+    int pkinit_dh_min_bits;
+    /* XXX Turn these into bit-fields */
+    int pkinit_require_binding;
+    int pkinit_allow_proxy_certs;
+    int synthetic_clients;
+    int pkinit_max_life_from_cert_extension;
+    krb5_timestamp pkinit_max_life_from_cert;
+    krb5_timestamp pkinit_max_life_bound;
+    krb5_timestamp synthetic_clients_max_life;
+    krb5_timestamp synthetic_clients_max_renew;
+
+    int enable_digest;
+    int digests_allowed;
+
+    int enable_gss_preauth;
+    int enable_gss_auth_data;
+    gss_OID_set gss_mechanisms_allowed;
+    gss_OID_set gss_cross_realm_mechanisms_allowed;
+
+    size_t max_datagram_reply_length;
+
+    int enable_kx509;
+};
+
 struct astgs_request_desc {
     ASTGS_REQUEST_DESC_COMMON_ELEMENTS;
 

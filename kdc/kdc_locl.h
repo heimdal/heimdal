8852ae51575abfc4d9e74862f2364fd6b7a83110
Luke Howard <lukeh@padl.com>
2022-01-17 12:14:14 +1100

    kdc: use bitfields for krb5_kdc_configuration bools

--- kdc/kdc_locl.h
+++ kdc/kdc_locl.h
@@ -67,15 +67,15 @@ struct kdc_request_desc {
 struct kdc_patypes;
 
 typedef struct krb5_kdc_configuration {
-    krb5_boolean require_preauth; /* require preauth for all principals */
-    time_t kdc_warn_pwexpire; /* time before expiration to print a warning */
-
     struct HDB **db;
-    int num_db;
+    size_t num_db;
 
     int num_kdc_processes;
 
-    krb5_boolean encode_as_rep_as_tgs_rep; /* bug compatibility */
+    time_t kdc_warn_pwexpire; /* time before expiration to print a warning */
+
+    unsigned int require_preauth : 1; /* require preauth for all principals */
+    unsigned int encode_as_rep_as_tgs_rep : 1; /* bug compatibility */
 
     /*
      * Windows 2019 (and earlier versions) always sends the salt
@@ -83,27 +83,27 @@ typedef struct krb5_kdc_configuration {
      * Samba AD DC will set this flag to match the AS-REP packet
      * exactly.
      */
-    krb5_boolean force_include_pa_etype_salt;
+    unsigned int force_include_pa_etype_salt : 1;
 
-    krb5_boolean tgt_use_strongest_session_key;
-    krb5_boolean preauth_use_strongest_session_key;
-    krb5_boolean svc_use_strongest_session_key;
-    krb5_boolean use_strongest_server_key;
+    unsigned int tgt_use_strongest_session_key : 1;
+    unsigned int preauth_use_strongest_session_key : 1;
+    unsigned int svc_use_strongest_session_key : 1;
+    unsigned int use_strongest_server_key : 1;
 
-    krb5_boolean check_ticket_addresses;
-    krb5_boolean warn_ticket_addresses;
-    krb5_boolean allow_null_ticket_addresses;
-    krb5_boolean allow_anonymous;
-    krb5_boolean historical_anon_realm;
-    krb5_boolean strict_nametypes;
+    unsigned int check_ticket_addresses : 1;
+    unsigned int warn_ticket_addresses : 1;
+    unsigned int allow_null_ticket_addresses : 1;
+    unsigned int allow_anonymous : 1;
+    unsigned int historical_anon_realm : 1;
+    unsigned int strict_nametypes : 1;
     enum krb5_kdc_trpolicy trpolicy;
 
-    krb5_boolean require_pac;
-    krb5_boolean enable_armored_pa_enc_timestamp;
-    krb5_boolean enable_unarmored_pa_enc_timestamp;
+    unsigned int require_pac : 1;
+    unsigned int enable_armored_pa_enc_timestamp : 1;
+    unsigned int enable_unarmored_pa_enc_timestamp : 1;
 
-    krb5_boolean enable_pkinit;
-    krb5_boolean pkinit_princ_in_cert;
+    unsigned int enable_pkinit : 1;
+    unsigned int pkinit_princ_in_cert : 1;
     const char *pkinit_kdc_identity;
     const char *pkinit_kdc_anchors;
     const char *pkinit_kdc_friendly_name;
@@ -111,11 +111,10 @@ typedef struct krb5_kdc_configuration {
     char **pkinit_kdc_cert_pool;
     char **pkinit_kdc_revoke;
     int pkinit_dh_min_bits;
-    /* XXX Turn these into bit-fields */
-    int pkinit_require_binding;
-    int pkinit_allow_proxy_certs;
-    int synthetic_clients;
-    int pkinit_max_life_from_cert_extension;
+    unsigned int pkinit_require_binding : 1;
+    unsigned int pkinit_allow_proxy_certs : 1;
+    unsigned int synthetic_clients : 1;
+    unsigned int pkinit_max_life_from_cert_extension : 1;
     krb5_timestamp pkinit_max_life_from_cert;
     krb5_timestamp pkinit_max_life_bound;
     krb5_timestamp synthetic_clients_max_life;
@@ -123,17 +122,17 @@ typedef struct krb5_kdc_configuration {
 
     krb5_log_facility *logf;
 
-    int enable_digest;
     int digests_allowed;
+    unsigned int enable_digest : 1;
 
-    int enable_gss_preauth;
-    int enable_gss_auth_data;
+    unsigned int enable_kx509 : 1;
+
+    unsigned int enable_gss_preauth : 1;
+    unsigned int enable_gss_auth_data : 1;
     gss_OID_set gss_mechanisms_allowed;
     gss_OID_set gss_cross_realm_mechanisms_allowed;
 
     size_t max_datagram_reply_length;
-
-    int enable_kx509;
 
     const char *app;
 } krb5_kdc_configuration;

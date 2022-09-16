1e67fd6c01e7bdc47cdfdec71cf202bdf1d88c05
Jeffrey Altman <jaltman@auristor.com>
2022-09-16 16:31:39 -0400

    krb5: rfc3526-MODP-group14 incorrect bit size
    Heimdal supports the 2 mandatory MODP groups (group 2 and group 14)
    according to RFC4556, however group 14 is defined with a size of
    1760 bits instead of 2048.
    
    Fixes #1001
    
    Reported-by: Julien Rische (GitHub: jrisc)

--- lib/krb5/pkinit.c
+++ lib/krb5/pkinit.c
@@ -2164,7 +2164,7 @@ static const char *default_moduli_rfc3526_MODP_group14 =
     /* name */
     "rfc3526-MODP-group14 "
     /* bits */
-    "1760 "
+    "2048 "
     /* p */
     "FFFFFFFF" "FFFFFFFF" "C90FDAA2" "2168C234" "C4C6628B" "80DC1CD1"
     "29024E08" "8A67CC74" "020BBEA6" "3B139B22" "514A0879" "8E3404DD"

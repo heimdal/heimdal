5838d3338a38fa8db1d20434443a0b9c663fb7d6
Nicolas Williams <nico@twosigma.com>
2022-10-26 00:02:22 -0500

    wind: Fix warning in test

--- lib/wind/test-normalize.c
+++ lib/wind/test-normalize.c
@@ -155,9 +155,13 @@ main(int argc, char **argv)
     if (f == NULL) {
 	const char *srcdir = getenv("srcdir");
 	if (srcdir != NULL) {
-	    char longname[256];
-	    snprintf(longname, sizeof(longname), "%s/%s", srcdir, filename);
+	    char *longname = NULL;
+
+	    if (asprintf(&longname, "%s/%s", srcdir, filename) == -1 ||
+                longname == NULL)
+                errx(1, "Out of memory");
 	    f = fopen(longname, "r");
+            free(longname);
 	}
 	if (f == NULL)
 	    err(1, "open %s", filename);

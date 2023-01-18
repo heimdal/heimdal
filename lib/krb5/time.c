286f580c3bbebd1569c6dabaada868456b675c4a
Nicolas Williams <nico@twosigma.com>
2023-01-18 00:54:00 -0600

    krb5: krb5_timeofday() should use gettimeofday()

--- lib/krb5/time.c
+++ lib/krb5/time.c
@@ -83,7 +83,10 @@ KRB5_LIB_FUNCTION krb5_error_code KRB5_LIB_CALL
 krb5_timeofday (krb5_context context,
 		krb5_timestamp *timeret)
 {
-    *timeret = time(NULL) + context->kdc_sec_offset;
+    struct timeval tv;
+
+    (void) gettimeofday(&tv, NULL);
+    *timeret = tv.tv_set + context->kdc_sec_offset;
     return 0;
 }
 

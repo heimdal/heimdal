64a2096cbb9f95454e274978b59c1458c854bcce
Joseph Sutton <josephsutton@catalyst.net.nz>
2023-03-08 15:22:29 +1300

    kdc-plugin: Split updating a PAC out of PAC verification
    Up to now krb5plugin_kdc_pac_verify() has served to both verified and
    updated a PAC. There are cases, however, when we only want to retrieve
    and verify a PAC, but don't want to modify it. This is the case with the
    PAC from a FAST armor ticket.
    
    Therefore, add a new plugin function, pac_update(), that will update a
    PAC obtained using pac_verify(). pac_verify() now only deals with
    verifying a PAC, while pac_update() handles any necessary updates to it.
    
    Signed-off-by: Joseph Sutton <josephsutton@catalyst.net.nz>

--- kdc/krb5tgs.c
+++ kdc/krb5tgs.c
@@ -96,6 +96,7 @@ _kdc_check_pac(astgs_request_t r,
     krb5_pac pac = NULL;
     krb5_error_code ret;
     krb5_boolean signedticket;
+    krb5_boolean is_trusted = FALSE;
 
     *kdc_issued = FALSE;
     *ppac = NULL;
@@ -125,7 +126,7 @@ _kdc_check_pac(astgs_request_t r,
     /* Verify the KDC signatures. */
     ret = _kdc_pac_verify(r,
 			  client_principal, delegated_proxy_principal,
-			  client, server, krbtgt, &pac);
+			  client, server, krbtgt, pac, &is_trusted);
     if (ret == 0) {
 	if (pac_canon_name) {
 	    ret = _krb5_pac_get_canon_principal(context, pac, pac_canon_name);
@@ -1924,6 +1925,29 @@ server_lookup:
 		spn, cpn, from, msg);
 	krb5_free_error_message(context, msg);
 	goto out;
+    }
+
+    if (priv->pac != NULL) {
+	ret = _kdc_pac_update(priv, priv->client_princ, NULL,
+			      priv->client, priv->server, priv->krbtgt,
+			      &priv->pac);
+	if (ret == KRB5_PLUGIN_NO_HANDLE) {
+	    ret = 0;
+	}
+	if (ret) {
+	    const char *msg = krb5_get_error_message(context, ret);
+	    kdc_audit_addreason((kdc_request_t)priv, "PAC update failed");
+	    kdc_log(context, config, 4,
+		    "Update PAC failed for %s (%s) from %s with %s",
+		    spn, cpn, from, msg);
+	    krb5_free_error_message(context, msg);
+	    goto out;
+	}
+
+	if (priv->pac == NULL) {
+	    /* the plugin may indicate no PAC should be generated */
+	    priv->pac_attributes = 0;
+	}
     }
 
     /*

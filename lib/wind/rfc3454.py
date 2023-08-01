d2041a6279c3c56163f44b4f6c21746f5d6c224a
Joseph Sutton <josephsutton@catalyst.net.nz>
2023-08-01 16:54:32 +1200

    wind: Do not use invalid escape sequences
    Signed-off-by: Joseph Sutton <josephsutton@catalyst.net.nz>

--- lib/wind/rfc3454.py
+++ lib/wind/rfc3454.py
@@ -47,13 +47,13 @@ def read(filename):
         if not l:
             break
         if inTable:
-            m = re.search('^ *----- End Table ([A-Z0-9\.]+) ----- *$', l)
+            m = re.search(r'^ *----- End Table ([A-Z0-9\.]+) ----- *$', l)
             if m:
                 ret[m.group(1)] = t
                 inTable = False
             else:
                 t.append(l)
-        if re.search('^ *----- Start Table ([A-Z0-9\.]+) ----- *$', l):
+        if re.search(r'^ *----- Start Table ([A-Z0-9\.]+) ----- *$', l):
             inTable = True
             t = []
     f.close()

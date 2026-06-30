6bb4ff842dc0b8e7be242cf9434d74fad528edf5
Roland C. Dowdeswell <elric@imrryr.org>
2026-06-30 13:13:37 +0100

    lib/roken: fix hex digit table initializer

--- lib/roken/hex.c
+++ lib/roken/hex.c
@@ -37,7 +37,10 @@
 #include <ctype.h>
 #include "hex.h"
 
-static const char hexchar[16] = "0123456789ABCDEF";
+static const char hexchar[16] = {
+    '0', '1', '2', '3', '4', '5', '6', '7',
+    '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
+};
 
 static inline int
 pos(char c)
